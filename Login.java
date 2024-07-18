0b4080f811ac98da0eaf88b57d1e6b3077c34aa9
NuAmanbaev <115341676+NuAmanbaev@users.noreply.github.com>
2024-07-17 19:49:38 -0500

    Nurga attempt

--- Login.java
+++ Login.java
@@ -2,4 +2,6 @@
 public class PrintPage{public static void main(String[] args) {
 	System.out.println("Hello, world");
 	System.out.println("Practicing fetch");
+	System.out.println("Nurga Attempt");
+	
     }}
